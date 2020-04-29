26347733398d18708330b0066138737b4e64ac1e
ashwin-sekar <ashwinsekar14@gmail.com>
2020-04-29 17:35:41 +0530

    included jquery

--- includes/Header.php
+++ includes/Header.php
@@ -24,6 +24,7 @@ else {
     <title>Octave</title>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <script src="assets/js/script.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 <body>
     <script>
