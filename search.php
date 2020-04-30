e896f434d029e8e13e5d80b0e7bf71149e5841f6
ashwin-sekar <ashwinsekar14@gmail.com>
2020-05-01 01:04:49 +0530

    fixed

--- search.php
+++ search.php
@@ -10,7 +10,7 @@ else {
 ?>
 <div class="searchContainer">
     <h4>Search for an artist, album or song</h4>
-    <input type="text" class="searchInput" onfocus="this.value()=this.value()" value="<?php echo $term; ?>" onfoplaceholder="Start typing...">
+    <input type="text" class="searchInput" onfocus="this.value = this.value" value="<?php echo $term; ?>" placeholder="Start typing...">
 </div>
 
 
